3c386df366ff9d6b4e4226d99501cccacec870d2
Nathan Golightly <n8golightly@gmail.com>
2022-09-14 15:45:43 -0500

    Automatically run every __ milliseconds

--- server.js
+++ server.js
@@ -2,41 +2,47 @@ const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-const URL = 'https://www.dallascowboys.com/';
-
-request(URL, function (err, res, body) {
-  if (err) {
-    console.log('Error!!!');
-  } else {
-    let $ = cheerio.load(body);
-
-    let names = '';
-    $(
-      '.nfl-c-media-playlist__aside-item > .d3-o-media-object__body > .d3-o-media-object__title'
-    ).each((i, el) => {
-      let name = $(el).text();
-      names += name;
-    });
-
-    fs.readFile('/Users/mergtech/tutorJobs/data.txt', 'utf8', (err, data) => {
+const detector = function (interval, URL, elements, nodefsData) {
+  setInterval(() => {
+    request(URL, function (err, res, body) {
       if (err) {
-        console.error(err);
-        return;
-      }
-
-      if (names === data) {
-        console.log('No change in names');
+        console.log('Error!!!');
       } else {
-        fs.writeFile('data.txt', names, function (err) {
+        let $ = cheerio.load(body);
+
+        let names = '';
+        $(elements).each((i, el) => {
+          let name = $(el).text();
+          names += name;
+        });
+
+        fs.readFile(nodefsData, 'utf8', (err, data) => {
           if (err) {
-            console.log(err);
+            console.error(err);
+            return;
+          }
+          if (names === data) {
+            console.log('No change in names');
           } else {
-            console.log(names);
+            fs.writeFile('data.txt', names, function (err) {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log(names);
+              }
+            });
           }
         });
       }
     });
-  }
-});
+  }, interval);
+};
 
-// const name = $(this).find('div._1-2Iqu>div.col-7-12>div._3wU53n').text();
+detector(
+  60000,
+  'https://www.dallascowboys.com/',
+  '.nfl-c-media-playlist__aside-item > .d3-o-media-object__body > .d3-o-media-object__title',
+  '/Users/mergtech/tutorJobs/data.txt'
+);
+
+// sample if needed: const name = $(this).find('div._1-2Iqu>div.col-7-12>div._3wU53n').text();
